f631f265e420443a319ef6a3d914138d7bee460f
giuseppefantoni-hotmart <giuseppe.agostini@hotmart.com>
2023-08-21 11:49:44 -0300

    feat: remove dist

--- dist/databases/index.js
+++
@@ -1,9 +0,0 @@
-export var dbConnection = {
-    url: "mongodb+srv://user2000:1579499@tcc-connect.fv5i7ho.mongodb.net/?retryWrites=true&w=majority",
-    options: {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-};
-
-//# sourceMappingURL=index.js.map
