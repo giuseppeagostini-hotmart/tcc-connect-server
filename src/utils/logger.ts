5d404c8b998a146e64c0c5701c117493d063022d
giuseppefantoni-hotmart <giuseppe.agostini@hotmart.com>
2023-08-21 12:20:48 -0300

    feat: update master

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,6 @@
 import { existsSync, mkdirSync } from 'fs';
 import { join } from 'path';
 import winston from 'winston';
-import winstonDaily from 'winston-daily-rotate-file';
 import { LOG_DIR } from '@config';
 
 const logDir: string = join(__dirname, LOG_DIR);
@@ -26,26 +25,6 @@ const logger = winston.createLogger({
   ),
   transports: [
     // debug log setting
-    new winstonDaily({
-      level: 'debug',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/debug', // log file /logs/debug/*.log in save
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30 Days saved
-      json: false,
-      zippedArchive: true,
-    }),
-    // error log setting
-    new winstonDaily({
-      level: 'error',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/error', // log file /logs/error/*.log in save
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30 Days saved
-      handleExceptions: true,
-      json: false,
-      zippedArchive: true,
-    }),
   ],
 });
 
